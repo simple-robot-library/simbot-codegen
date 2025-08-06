58e4a684914f71532a109fb7dd616cfcfbd617ff
ForteScarlet <ForteScarlet@163.com>
2025-08-06 17:55:51 +0800

    更新一波

--- composeApp/src/wasmJsMain/kotlin/love/forte/simbot/codegen/gen/view/DownloadComponents.kt
+++ composeApp/src/wasmJsMain/kotlin/love/forte/simbot/codegen/gen/view/DownloadComponents.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import js.errors.toJsErrorLike
 import js.objects.unsafeJso
 import js.promise.await
 import jszip.JSZipGeneratorOptions
@@ -25,6 +26,7 @@ import love.forte.simbot.codegen.filesaver.saveAs
 import love.forte.simbot.codegen.gen.GradleProjectViewModel
 import love.forte.simbot.codegen.gen.doGenerate
 import web.blob.Blob
+import web.console.console
 
 /**
  * 下载按钮组件，用于生成并下载项目
@@ -47,7 +49,7 @@ fun DoDownload(
                 kotlin.runCatching {
                     doDownload(project)
                 }.onFailure { err ->
-                    println("生成失败: $err")
+                    console.error("生成失败".toJsString(), err.toJsErrorLike())
                     window.alert("生成失败QAQ")
                 }
             }.invokeOnCompletion { loadingCounter.dec() }
