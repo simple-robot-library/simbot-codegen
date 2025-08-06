01c830185cc79c9fa389540e3192dd8d083819cc
ForteScarlet <ForteScarlet@163.com>
2025-08-06 18:07:11 +0800

    预定义一些names

--- composeApp/src/wasmJsMain/kotlin/love/forte/simbot/codegen/codegen/naming/SimbotNames.kt
+++ composeApp/src/wasmJsMain/kotlin/love/forte/simbot/codegen/codegen/naming/SimbotNames.kt
@@ -1,2 +1,22 @@
 package love.forte.simbot.codegen.codegen.naming
 
+import love.forte.codegentle.common.naming.ClassName
+import love.forte.codegentle.common.naming.PackageName
+import love.forte.codegentle.common.naming.parseToPackageName
+import love.forte.codegentle.common.naming.plus
+
+// TODO
+object SimbotNames {
+    val simbotPkg = "love.forte.simbot".parseToPackageName()
+    val simbotEventPkg = simbotPkg + "event"
+    val simbotSpringPkg = simbotPkg + "spring"
+    val simbotQuantcatCommonAnoPkg = simbotPkg + "quantcat" + "common" + "annotations"
+
+    val listenerAno = ClassName(simbotQuantcatCommonAnoPkg, "Listener")
+    val filterAno = ClassName(simbotQuantcatCommonAnoPkg, "Filter")
+    val enableSimbotAno = ClassName(simbotSpringPkg, "EnableSimbot")
+    val contentTrimAno = ClassName(simbotQuantcatCommonAnoPkg, "ContentTrim")
+    val eventClassName = ClassName(simbotEventPkg, "Event")
+    val msgEventClassName = ClassName(simbotEventPkg, "MessageEvent")
+    val contactMsgEventClassName = ClassName(simbotEventPkg, "ContactMessageEvent")
+}
---
+++ composeApp/src/wasmJsMain/kotlin/love/forte/simbot/codegen/codegen/naming/SpringNames.kt
@@ -0,0 +1,8 @@
+package love.forte.simbot.codegen.codegen.naming
+
+import love.forte.codegentle.common.naming.ClassName
+
+// TODO
+object SpringNames {
+    private val componentAno = ClassName("org.springframework.stereotype", "Component")
+}
